b00fc45258730122a771908dfd550738fc49d116
Alan <alan.sky2333@gmail.com>
2024-05-02 12:39:58 +0200

    parallelized COMSOL simulation setup
    apply the load on each core

--- comsol_interface.py
+++ comsol_interface.py
@@ -1,8 +1,9 @@
+from bz2 import compress
 import mph
- 
+
 class ACC_ComsolInterface:
     def __init__(self, model_path):
-        self.client = mph.start() # Start the COMSOL client
+        self.client = mph.start(cores=1) # Start the COMSOL client
         try:
             self.model = self.client.load(model_path)
             self.model_path = model_path
@@ -31,5 +32,34 @@ class ACC_ComsolInterface:
         self.set_parameter('H', solution[0])
         self.set_parameter('Thickn', solution[1])
         self.set_parameter('W', solution[2])
-        print(self.model.parameters())
+        return self.model
+    
+class Speaker_2D_ComsolInterface:
+    def __init__(self, model_path):
+        self.client = mph.start(cores=1) # Start the COMSOL client
+        try:
+            self.model = self.client.load(model_path)
+            self.model_path = model_path
+            print("Loaded model:",self.client.models())
+        except Exception as e:
+            print("Error: Could not load the model.",e)
+            exit()
+
+    def run_simulation(self):
+        self.model.mesh() # Mesh the geometry
+        self.model.solve() # normally there is only one study
+        return self.model
+
+    def clear(self):
+        self.model.clear() # Clear disk space for history and results
+    
+    def get_displacement(self):
+        return self.model.evaluate('')
+    
+    def set_parameter(self, name, value):
+        self.model.parameter(name=name, value=value) # Set the parameter of the model, name is the name of the parameter, value is the value of the parameter
+        self.model.build()  # Build the model after setting the parameter
+        return self.model
+    
+    def update(self, solution): # update the model with the solution, specifically the height, thickness and width of the beam
         return self.model
--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import pygad
 import numpy
 import multiprocessing
-
 from comsol_interface import ACC_ComsolInterface
 
-model = ACC_ComsolInterface('3D_cantilever_beam.mph')
-
-upper_bound = [0.3,0.01,0.03]
-lower_bound = [0.1,0.001,0.01]   # they are height, thickness and width of the beam respectively, make sure the alignment is correct
+CPU_count = 24
+upper_bound = [0.5,0.01,0.03]
+lower_bound = [0.15,0.001,0.01]   # they are height, thickness and width of the beam respectively, make sure the alignment is correct
 gene_space = [{'low': lower_bound[i], 'high': upper_bound[i]} for i in range(len(upper_bound))]
-initial_values = [0.2, 0.005, 0.02]
+# think about discretizing the gene space to make the search more efficient
 
+pool = None
 
-def fitness_func(ga_instance, solution, solution_idx):    
+def worker_init():
+    global model 
+    model = ACC_ComsolInterface('3D_cantilever_beam.mph')
+
+def initialize_pool():
+    global pool
+    pool = multiprocessing.Pool(processes=CPU_count, initializer=worker_init)
+
+def worker_job(solution):
+    print(f"Processing job for solution = {solution}")
     model.update(solution)  # Update the model with the solution
-    model.run_simulation()  # include build, mesh and solve
-    displacement = model.get_displacement()
-    fitness_value = max(displacement) # simply take the maximum displacement of the beam
-    print(f"Solution {solution_idx} fitness = {fitness_value}")
-    return fitness_value  # Fitness value is the maximum displacement of the beam 
-    
-    """
-    # This is an example of a fitness function that can be used with the GA, as this tedency is an example of beam
-def fitness_func_test(ga_instance, solution, solution_idx):
-    fitness_value = 10*solution[0] - 2*solution[1] + 0.1*solution[2] # height has 10 times more impact than thickness, and thickness has 20 times more impact than width
-    return fitness_value
-    """
+    model.run_simulation()  # include build, mesh and solve 
+    return max(model.get_displacement()) # simply take the maximum displacement of the beam
+
+def fitness_func(ga_instance, solutions, solutions_idx):
+    global pool
+    if pool is None:
+        initialize_pool()
+    fitness_values = pool.map(worker_job, solutions) # type: ignore #dynamically changing the type of the pool
+    return fitness_values
 
 last_fitness = 0
 def on_generation(ga_instance):
@@ -35,24 +40,28 @@ def on_generation(ga_instance):
     print(f"Change     = {ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - last_fitness}")
     last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
 
-num_generations = 50
-num_parents_mating = 4
+num_generations = 20
+num_parents_mating = 16
 
-sol_per_pop = 8
+sol_per_pop = CPU_count
 num_genes = len(upper_bound)
 
 ga_instance = pygad.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
+                       fitness_batch_size=CPU_count,
                        fitness_func=fitness_func,
                        sol_per_pop=sol_per_pop,
                        num_genes=num_genes,
                        gene_space=gene_space,
-                       on_generation=on_generation)
+                       on_generation=on_generation,
+                       # parallel_processing=24, not able to use, error: Cannot access client instance from different thread. probably there are some crosstalk between the threads of the comsol 
+                       save_solutions=True)
 
-ga_instance.run()
-ga_instance.plot_fitness()
-
-# Returning the details of the best solution.
-solution, solution_fitness, solution_idx = ga_instance.best_solution()
-print("Parameters of the best solution : {solution}".format(solution=solution))
-print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+if __name__ == '__main__':
+    ga_instance.run()
+    ga_instance.plot_fitness()
+    ga_instance.plot_genes()
+    ga_instance.plot_new_solution_rate()
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
