253d126e6190c490a95d61d3c8bdc2031fea644b
Alan <alan.sky2333@gmail.com>
2024-05-20 23:49:04 +0200

    milestone finished
    Existing problems: 1. Don't know if the get_FOM method evaluate the correct values. 2. Don't know why some results can't be obtained from the COSMOL simulations.

--- comsol_interface.py
+++ comsol_interface.py
@@ -1,4 +1,3 @@
-from bz2 import compress
 import mph
 
 class ACC_ComsolInterface:
@@ -7,7 +6,7 @@ class ACC_ComsolInterface:
         try:
             self.model = self.client.load(model_path)
             self.model_path = model_path
-            print("Loaded model:",self.client.models())
+            print("Loaded model on one core:",self.client.models())
         except Exception as e:
             print("Error: Could not load the model.",e)
             exit()
@@ -35,12 +34,13 @@ class ACC_ComsolInterface:
         return self.model
     
 class Speaker_2D_ComsolInterface:
+    frequency_steps = 40 # the number of frequency steps in the simulation. Change if the model study steps is changed
+
     def __init__(self, model_path):
         self.client = mph.start(cores=1) # Start the COMSOL client
         try:
             self.model = self.client.load(model_path)
             self.model_path = model_path
-            print("Loaded model:",self.client.models())
         except Exception as e:
             print("Error: Could not load the model.",e)
             exit()
@@ -53,13 +53,38 @@ class Speaker_2D_ComsolInterface:
     def clear(self):
         self.model.clear() # Clear disk space for history and results
     
-    def get_displacement(self):
-        return self.model.evaluate('')
-    
+    def get_disp_FOM(self):
+        displacement = self.model.evaluate('solid.disp','mm') # as we know the frequency simulation range is from 100Hz to 80kHz with step size of 500Hz, so we can have a FOM from 20Hz to 20kHz
+        FOM = sum(displacement[0:self.frequency_steps][:,0]) # select displacement from 20Hz to 20kHz, and sum them up as integral, first column is the spatial coordinator at (0,0,0) 
+        return FOM
+
     def set_parameter(self, name, value):
         self.model.parameter(name=name, value=value) # Set the parameter of the model, name is the name of the parameter, value is the value of the parameter
         self.model.build()  # Build the model after setting the parameter
         return self.model
     
+    def get_current_FOM(self, solution): # the current of the ALN
+        r_min, r_max = 0, solution[1]*solution[4] # calculate the min and max radius of the ALN
+        z_min, z_max = solution[0] + solution[3], solution[0] + solution[2] + solution[3] # calculate the min and max height of the piezoelectric material
+        [r,z,current] = self.model.evaluate(['r','z','es.normJ']) # here it return the A/m^2 all across the surface, choose the surface on pizeoelectric material to get the charge
+        mask = [(r[0:self.frequency_steps] >= r_min) & (r[0:self.frequency_steps] <= r_max) & (z[0:self.frequency_steps] >= z_min) & (z[0:self.frequency_steps] <= z_max)]
+        filtered_current = current[0:self.frequency_steps][mask[0]]
+        FOM = sum(filtered_current) # sum up the current to get the figure of merit in frequency 20Hz to 20kHz
+        return FOM
+    
     def update(self, solution): # update the model with the solution, specifically the height, thickness and width of the beam
+        self.set_parameter('t_silicon',f'{solution[0]}[um]')
+        self.set_parameter('radius',f'{solution[1]}[um]')
+        self.set_parameter('t_AlN',f'{solution[2]}[um]')
+        self.set_parameter('t_SiO2',f'{solution[3]}[um]')
+        self.set_parameter('r_electrode',f'{(solution[4]*solution[1])}[um]')
         return self.model
+    
+# test code for the data read and set functions
+if __name__ == '__main__':
+    model = Speaker_2D_ComsolInterface('2D_Piezoelectric_Microphone_for_GA_based_Optimization.mph')
+    solution = [10, 1000, 0.6, 0.5, 0.8]
+    model.run_simulation()
+    model.update(solution)
+    print(model.model.parameters()) # print the parameters of the model
+    print(model.get_current_FOM(solution))
--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import pygad
-import numpy
 import multiprocessing
-from comsol_interface import ACC_ComsolInterface
+from comsol_interface import ACC_ComsolInterface, Speaker_2D_ComsolInterface
 
-CPU_count = 24
-upper_bound = [0.5,0.01,0.03]
-lower_bound = [0.15,0.001,0.01]   # they are height, thickness and width of the beam respectively, make sure the alignment is correct
-gene_space = [{'low': lower_bound[i], 'high': upper_bound[i]} for i in range(len(upper_bound))]
-# think about discretizing the gene space to make the search more efficient
+CPU_count = 20
+upper_bound = [15,7000,3,2,0.8]
+lower_bound = [4,1000,0.5,0.5,0.2]   # they are t_silicon, radius, t_AlN, t_SiO2, electrode ratio respectively, make sure the alignment is correct
+step_values = [1,10,0.1,0.1,0.1] # set the discrete values for faster convergence
+gene_space = [{'low': lower_bound[i], 'high': upper_bound[i], 'step': step_values[i]} for i in range(len(upper_bound))]
 
+# The section below is the parallelization of the optimization process
+# Since the load mph is quite time consuming, the model loading is done in the worker_init function
+# NOTICE: don't edit the parallization, Python is quite weak in supporting parallelization, so the code below is quite tricky
 pool = None
 
 def worker_init():
     global model 
-    model = ACC_ComsolInterface('3D_cantilever_beam.mph')
+    model = Speaker_2D_ComsolInterface('2D_Piezoelectric_Microphone_for_GA_based_Optimization.mph')
 
 def initialize_pool():
     global pool
     pool = multiprocessing.Pool(processes=CPU_count, initializer=worker_init)
+    print("Process pool has been set up.")
 
 def worker_job(solution):
-    print(f"Processing job for solution = {solution}")
-    model.update(solution)  # Update the model with the solution
-    model.run_simulation()  # include build, mesh and solve 
-    return max(model.get_displacement()) # simply take the maximum displacement of the beam
+    try:
+        model.update(solution)  # Update the model with the solution
+        model.run_simulation()  # include build, mesh and solve 
+        FOM = model.get_disp_FOM()  # get the figure of merit
+    except:
+        print(f"Error for solution = {solution}") # print if the error occurs
+        FOM = 0 # return 0 if there is an error, to avoid any situations like COMSOL model not converging, it's a way to abandon these values
+    return FOM
+
+def shutdown_pool():
+    global pool
+    if pool is not None:
+        pool.close()
+        pool.join()
+        pool = None
+    print("Process pool has been shut down.")
 
 def fitness_func(ga_instance, solutions, solutions_idx):
     global pool
+    print(f"Processing solutions = {solutions_idx}")
     if pool is None:
         initialize_pool()
-    fitness_values = pool.map(worker_job, solutions) # type: ignore #dynamically changing the type of the pool
+    fitness_values = pool.map(worker_job,solutions) # type: ignore #dynamically changing the type of the pool
     return fitness_values
 
 last_fitness = 0
@@ -40,8 +56,8 @@ def on_generation(ga_instance):
     print(f"Change     = {ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1] - last_fitness}")
     last_fitness = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)[1]
 
-num_generations = 20
-num_parents_mating = 16
+num_generations = 100
+num_parents_mating = 12
 
 sol_per_pop = CPU_count
 num_genes = len(upper_bound)
@@ -54,8 +70,9 @@ ga_instance = pygad.GA(num_generations=num_generations,
                        num_genes=num_genes,
                        gene_space=gene_space,
                        on_generation=on_generation,
-                       # parallel_processing=24, not able to use, error: Cannot access client instance from different thread. probably there are some crosstalk between the threads of the comsol 
-                       save_solutions=True)
+                       suppress_warnings=True,
+                       save_solutions=True
+                       )
 
 if __name__ == '__main__':
     ga_instance.run()
@@ -65,3 +82,4 @@ if __name__ == '__main__':
     solution, solution_fitness, solution_idx = ga_instance.best_solution()
     print("Parameters of the best solution : {solution}".format(solution=solution))
     print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+    shutdown_pool()
